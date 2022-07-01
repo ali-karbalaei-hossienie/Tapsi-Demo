bd288c968c82b0eefb00983f3e64e2b3f16ec5a2
ali-karbalaei-hossienie <mpkarbalaei@gmail.com>
2022-07-02 01:04:08 +0430

    Add Avatar , Add Accordion , Add Radio-Button

--- js/main.js
+++ js/main.js
@@ -1,30 +1,37 @@
-const toggler = document.querySelector(".nav__toggler");
-const navbar = document.querySelector(".nav");
-toggler.addEventListener("click", (e) => {
-  console.log("click");
-  navbar.classList.toggle("nav__expanded");
-});
+// const toggler = document.querySelector(".nav__toggler");
+// const navbar = document.querySelector(".nav");
+// toggler.addEventListener("click", (e) => {
+//   console.log("click");
+//   navbar.classList.toggle("nav__expanded");
+// });
 
-const servicesTabs = document.querySelector(".services__tabs");
-const TabsContent = document.querySelectorAll(".tab-content");
-const Tabs = document.querySelectorAll(".tab");
+// const servicesTabs = document.querySelector(".services__tabs");
+// const TabsContent = document.querySelectorAll(".tab-content");
+// const Tabs = document.querySelectorAll(".tab");
 
-servicesTabs.addEventListener("click", function (e) {
-  const click = e.target.closest(".tab");
-  if (!click) return;
+// servicesTabs.addEventListener("click", function (e) {
+//   const click = e.target.closest(".tab");
+//   if (!click) return;
 
-  // remove active
-  Tabs.forEach((t) => t.classList.remove("active"));
-  TabsContent.forEach((content) => {
-    content.classList.remove("active");
-  });
+//   // remove active
+//   Tabs.forEach((t) => t.classList.remove("active"));
+//   TabsContent.forEach((content) => {
+//     content.classList.remove("active");
+//   });
 
-  // Add active
-  click.classList.add("active");
-  TabsContent.forEach((content) => {
-    const targeTabContent = document.querySelector(
-      `#${click.dataset.tabTarget}`
-    );
-    targeTabContent.classList.add("active");
-  });
+//   // Add active
+//   click.classList.add("active");
+//   TabsContent.forEach((content) => {
+//     const targeTabContent = document.querySelector(
+//       `#${click.dataset.tabTarget}`
+//     );
+//     targeTabContent.classList.add("active");
+//   });
+// });
+
+const accordianWrapper = document.querySelector(".accordian__wrapper");
+accordianWrapper.addEventListener("click", function (e) {
+  const accordian = e.target.closest(".accordian");
+  if (!accordian) return;
+  accordian.classList.toggle("accordian__expand");
 });
